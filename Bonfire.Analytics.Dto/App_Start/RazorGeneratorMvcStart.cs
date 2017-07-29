6b1a3c3c8339b26fa64821939151a44d589a1a80
Chris Auer <cja@buildabonfire.com>
2017-07-29 00:12:10 -0400

    Updated to 8.2 Updsate 4

---
+++ Bonfire.Analytics.Dto/App_Start/RazorGeneratorMvcStart.cs
@@ -0,0 +1,21 @@
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using RazorGenerator.Mvc;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(Bonfire.Analytics.Dto.RazorGeneratorMvcStart), "Start")]
+
+namespace Bonfire.Analytics.Dto {
+    public static class RazorGeneratorMvcStart {
+        public static void Start() {
+            var engine = new PrecompiledMvcEngine(typeof(RazorGeneratorMvcStart).Assembly) {
+                UsePhysicalViewsIfNewer = HttpContext.Current.Request.IsLocal
+            };
+
+            ViewEngines.Engines.Insert(0, engine);
+
+            // StartPage lookups are done by WebPages. 
+            VirtualPathFactoryManager.RegisterVirtualPathFactory(engine);
+        }
+    }
+}

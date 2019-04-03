925bbd2a6df5d943fb24866a08729a84aacd2720
Chris Auer <cja@buildabonfire.com>
2019-04-03 17:32:02 -0400

    Cleanup

--- Bonfire.Analytics.Dto/Controllers/VisitorController.cs
+++ Bonfire.Analytics.Dto/Controllers/VisitorController.cs
@@ -27,35 +27,6 @@ namespace Bonfire.Analytics.Dto.Controllers
         {
             var trackerDto = contactRepository.GetTrackerDto();
 
-            //var facets = new List<KeyValuePair<string, List<KeyValuePair<string, object>>>>();
-
-            //foreach (var contactFacet in trackerDto.Contact.Facets)
-            //{
-            //    var facetValues = new List<KeyValuePair<string, object>>();
-            //    var allprops = contactFacet.Value.GetType().GetProperties();
-
-            //    foreach (var propertyInfo in allprops)
-            //    {
-            //        if (propertyInfo.Name != "XObject")
-            //            facetValues.Add(new KeyValuePair<string, object>(propertyInfo.Name, propertyInfo.GetValue(contactFacet.Value)));
-            //    }
-
-            //    facets.Add(new KeyValuePair<string, List<KeyValuePair<string, object>>>(contactFacet.Key, facetValues));
-            //}
-
-
-
-
-            //var jsonResolver = new PropertyRenameAndIgnoreSerializerContractResolver();
-            //jsonResolver.IgnoreProperty(typeof(XdbExtensible), "XObject");
-            ////jsonResolver.RenameProperty(typeof(Person), "FirstName", "firstName");
-
-            //var serializerSettings = new JsonSerializerSettings();
-            //serializerSettings.ContractResolver = jsonResolver;
-
-            //var json = JsonConvert.SerializeObject(trackerDto.Contact, serializerSettings);
-
-
             return new JsonNet(trackerDto);
         }
 

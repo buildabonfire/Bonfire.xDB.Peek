63d07d9f28e97eb28f38c5fdcbac96a9f6d3bd77
Chris Auer <cja@buildabonfire.com>
2019-07-24 10:12:13 -0400

    Added MVC view for visitor details

---
+++ Bonfire.Analytics.Dto/Controllers/BonfireDtoController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Bonfire.Analytics.Dto.Repositories;
+
+namespace Bonfire.Analytics.Dto.Controllers 
+{
+    public class BonfireDtoController : Controller
+    {
+        private readonly IContactRepository contactRepository;
+
+        public BonfireDtoController(IContactRepository contactRepository)
+        {
+            this.contactRepository = contactRepository;
+        }
+
+        public ActionResult Details()
+        {
+            var model = contactRepository.GetTrackerDto();
+
+            return View(model);
+        }
+    }
+}
